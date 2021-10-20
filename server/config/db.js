11ac115c565e6cf2ff5e46cfbf92b393647e1607
grant <gmacmil3@my.centennialcollege.ca>
2021-10-20 19:56:23 -0400

    updated DB URI to work with Mongo Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = 
 {
-    "URI": "mongodb://localhost/assignment_contacts"
+    //"URI": "mongodb://localhost/assignment_contacts"
+    "URI": "mongodb+srv://grant:Macmilg2660@mongodbserver.a0xbg.mongodb.net/assignment_contacts?retryWrites=true&w=majority"
 }
